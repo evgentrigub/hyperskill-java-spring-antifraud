90371bd0d2ce042ad01a53f8e2a98c61da663704
Eugene Trigubov <trigubov.eugene@gmail.com>
2026-09-05 17:26:14 +0200

    init commit

---
+++ src/main/java/hyperskill/evgentrigub/antifraud/HyperskillAntifraudApplication.java
@@ -0,0 +1,13 @@
+package hyperskill.evgentrigub.antifraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperskillAntifraudApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(HyperskillAntifraudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hyperskill/evgentrigub/antifraud/HyperskillAntifraudApplicationTests.java
@@ -0,0 +1,13 @@
+package hyperskill.evgentrigub.antifraud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperskillAntifraudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
